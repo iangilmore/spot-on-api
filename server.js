26a8a08671b83f83c6bc38e0a9b1e8420aba96c0
Ian Gilmore <ian.gilmore@me.com>
2024-04-10 16:57:26 -0400

    add dynamic import of dev dependency

--- server.js
+++ server.js
@@ -2,25 +2,34 @@ import './config/database.js'
 
 import express from 'express'
 import cors from 'cors'
-import httpsLocalHost from 'https-localhost'
+// import httpsLocalHost from 'https-localhost'
 import cookieParser from 'cookie-parser'
 
 import usersRouter from './routes/usersRouter.js'
 
 let app
-const useLocalHost = () => { app = httpsLocalHost() }
-const useExpress = () => { app = express() }
-process.env.USE_HTTPS_LOCALHOST ? useLocalHost() : useExpress()
+async () => {
+  if (process.env.USE_HTTPS_LOCALHOST) {
+    const httpsLocalHost = await import('https-localhost');
+    app = httpsLocalHost()
+    allTheThings()
+  } else {
+    app = express()
+    allTheThings()
+  }
+}
 
-app.listen(443, () => {
-  console.log(`Express server is running`);
-})
-
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
-
-app.use('/user', usersRouter)
-app.use('/', async (req, res) => {
-  res.send('<h1>Spot On API</h1>')
-})
+function allTheThings() {
+  app.listen(443, () => {
+    console.log(`Express server is running`);
+  })
+  
+  app.use(express.json())
+  app.use(cors())
+  app.use(cookieParser())
+  
+  app.use('/user', usersRouter)
+  app.use('/', async (req, res) => {
+    res.send('<h1>Spot On API</h1>')
+  })
+}
