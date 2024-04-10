10c86e5784a2c0dd6faa40dbb502795d2f2e6dee
Ian Gilmore <ian.gilmore@me.com>
2024-04-10 18:02:02 -0400

    band-aid dev dependency issue

--- server.js
+++ server.js
@@ -2,34 +2,24 @@ import './config/database.js'
 
 import express from 'express'
 import cors from 'cors'
-// import httpsLocalHost from 'https-localhost'
 import cookieParser from 'cookie-parser'
 
 import usersRouter from './routes/usersRouter.js'
 
 let app
-async () => {
-  if (process.env.USE_HTTPS_LOCALHOST) {
-    const httpsLocalHost = await import('https-localhost');
-    app = httpsLocalHost()
-    allTheThings()
-  } else {
-    app = express()
-    allTheThings()
-  }
-}
+// import httpsLocalHost from 'https-localhost'
+// app = httpsLocalHost()
+app = express()
 
-function allTheThings() {
-  app.listen(process.env.PORT, () => {
-    console.log(`Express server is running`);
-  })
-  
-  app.use(express.json())
-  app.use(cors())
-  app.use(cookieParser())
-  
-  app.use('/user', usersRouter)
-  app.use('/', async (req, res) => {
-    res.send('<h1>Spot On API</h1>')
-  })
-}
+app.listen(process.env.PORT, () => {
+  console.log(`Express server is running`);
+})
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use('/user', usersRouter)
+app.use('/', async (req, res) => {
+  res.send('<h1>Spot On API</h1>')
+})
