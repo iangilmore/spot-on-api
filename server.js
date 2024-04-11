746216bfc4c52d2a452577563563a1a396ad282e
Ian Gilmore <ian.gilmore@me.com>
2024-04-10 23:28:06 -0400

    attempt conditional import, scaffold additional routes

--- server.js
+++ server.js
@@ -5,21 +5,36 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 import usersRouter from './routes/usersRouter.js'
+// import puzzlesRouter from './routes/puzzlesRouter.js'
+// import puzzleCardsRouter from './routes/puzzleCardsRouter.js'
+// import resultsRouter from './routes/resultsRouter.js'
 
 let app
-// import httpsLocalHost from 'https-localhost'
-// app = httpsLocalHost()
-app = express()
+async () => {
+  if (process.env.USE_HTTPS_LOCALHOST === 'true') {
+    let httpsLocalHost = await import('https-localhost')
+    app = httpsLocalHost.default()
+    allTheThings()
+  } else {
+    app = express()
+    allTheThings()
+  }
+}
 
-app.listen(process.env.PORT, () => {
-  console.log(`Express server is running`);
-})
-
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
-
-app.use('/user', usersRouter)
-app.use('/', async (req, res) => {
-  res.send('<h1>Spot On API</h1>')
-})
+function allTheThings() {
+  app.listen(process.env.PORT, () => {
+    console.log(`Express server is running`);
+  })
+  
+  app.use(express.json())
+  app.use(cors())
+  app.use(cookieParser())
+  
+  app.use('/user', usersRouter)
+  // app.use('/puzzles', puzzlesRouter)
+  // app.use('/puzzleCards', puzzleCardsRouter)
+  // app.use('/results', resultsRouter)
+  app.use('/', async (req, res) => {
+    res.send('<h1>Spot On API</h1>')
+  })  
+}
