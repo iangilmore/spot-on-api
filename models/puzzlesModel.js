164e4fd14c7e7f1fcc95e345131265e8bec65060
Ian Gilmore <ian.gilmore@me.com>
2024-04-14 21:06:52 -0400

    add Puzzle schema validation logic to date field and virtual field logic to spotOnId

--- models/puzzlesModel.js
+++ models/puzzlesModel.js
@@ -2,10 +2,23 @@ import mongoose from "mongoose"
 
 const puzzleSchema = new mongoose.Schema({
   spotOnId: { type: String, required: true },
-  date: { type: Date, required: true },
+  date: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{8}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid date in YYYYMMDD format!`
+    }
+  },
   levelValue: { type: Number, required: true, min: 0, max: 2 },
   puzzleCards: { type: [mongoose.Schema.Types.ObjectId], ref: 'PuzzleCard' },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+})
+
+puzzleSchema.virtual('spotOnId').get(function () {
+  return `${this.date}-${this.levelValue}`
 })
 
 const Puzzle = mongoose.model("Puzzle", puzzleSchema)
